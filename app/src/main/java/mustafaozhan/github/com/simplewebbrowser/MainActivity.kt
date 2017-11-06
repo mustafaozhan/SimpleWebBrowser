1caff677a68c7729e7320ed73b233d4463654428
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-11-06 20:21:08 +0300

    Loading Url into webView is implemented

--- app/src/main/java/mustafaozhan/github/com/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/simplewebbrowser/MainActivity.kt
@@ -2,11 +2,37 @@ package mustafaozhan.github.com.simplewebbrowser
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        webView.loadUrl("http://google.com")
+
+        webView.webViewClient = MyWebClient()
+        button.setOnClickListener { load() }
+    }
+
+    @Suppress("OverridingDeprecatedMember")
+    class MyWebClient : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            view?.loadUrl(url)
+            return true
+        }
+
+    }
+
+    private fun load() {
+        val url = editText.text.toString()
+
+        if (url != "") {
+            webView.loadUrl(url)
+        }
     }
 }
